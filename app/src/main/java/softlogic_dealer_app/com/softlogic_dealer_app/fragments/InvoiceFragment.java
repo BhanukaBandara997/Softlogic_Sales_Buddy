3c1de03ac1c0fc6c213511b5903d8c29a231f9d1
Bhanuka Bandara <37652776+BhanukaBandara997@users.noreply.github.com>
2020-07-08 16:15:56 +0530

    Camera Permissions on Application Done

--- app/src/main/java/softlogic_dealer_app/com/softlogic_dealer_app/fragments/InvoiceFragment.java
+++ app/src/main/java/softlogic_dealer_app/com/softlogic_dealer_app/fragments/InvoiceFragment.java
@@ -6,23 +6,19 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.Fragment;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.Fragment;
 
 import softlogic_dealer_app.com.softlogic_dealer_app.R;
-
-import static androidx.core.content.PermissionChecker.checkSelfPermission;
 
 public class InvoiceFragment extends Fragment {
 
