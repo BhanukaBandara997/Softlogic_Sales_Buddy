6a78f79a8a4c8de8674fb7d5c53030044623550d
Bhanuka Bandara <37652776+BhanukaBandara997@users.noreply.github.com>
2020-07-08 16:15:46 +0530

    Camera Permissions on Application Done

--- app/src/main/java/softlogic_dealer_app/com/softlogic_dealer_app/fragments/InvoiceFragment.java
+++ app/src/main/java/softlogic_dealer_app/com/softlogic_dealer_app/fragments/InvoiceFragment.java
@@ -1,15 +1,28 @@
 package softlogic_dealer_app.com.softlogic_dealer_app.fragments;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import softlogic_dealer_app.com.softlogic_dealer_app.R;
+
+import static androidx.core.content.PermissionChecker.checkSelfPermission;
 
 public class InvoiceFragment extends Fragment {
 
@@ -21,6 +34,12 @@ public class InvoiceFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private static final int CAMERA_REQUEST = 1888;
+    private static final int MY_CAMERA_PERMISSION_CODE = 100;
+    EditText invoiceNumber, invoiceProductSerialNumber, invoiceProductQuantity;
+    Button attachInvoiceBtn, uploadInvoiceButton;
+    ImageView invoiceReceipt;
+
 
     public InvoiceFragment() {
         // Required empty public constructor
@@ -59,6 +78,46 @@ public class InvoiceFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle("Invoice Generator");
-        return inflater.inflate(R.layout.fragment_invoice, container, false);
+        View view = inflater.inflate(R.layout.fragment_invoice, container, false);
+        invoiceNumber = view.findViewById(R.id.invoice_number);
+        invoiceProductSerialNumber = view.findViewById(R.id.invoice_product_serial_number);
+        invoiceProductQuantity = view.findViewById(R.id.invoice_product_quantity);
+        attachInvoiceBtn = view.findViewById(R.id.attach_invoice_btn);
+        uploadInvoiceButton = view.findViewById(R.id.upload_invoice_button);
+        invoiceReceipt = view.findViewById(R.id.invoice_receipt);
+
+        attachInvoiceBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_PERMISSION_CODE);
+                } else {
+                    Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_REQUEST);
+                }
+            }
+        });
+        return view;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == MY_CAMERA_PERMISSION_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(cameraIntent, CAMERA_REQUEST);
+            } else {
+
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            invoiceReceipt.setImageBitmap(photo);
+        }
     }
 }
